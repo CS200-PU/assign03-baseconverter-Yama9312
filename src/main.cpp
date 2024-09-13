5d9658a8cf8e0f6b50617f77913de6ac2f7c3e97
Nathan Yamashiro <112516038+Yama9312@users.noreply.github.com>
2024-09-13 21:05:25 +0000

    starting

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 //******************************************************************************
 // File name:   main.cpp
-// Author:      CS Faculty
-// Date:        xx/xx/xxxx (Replace xx/xx/xxxx with month/day/year)
+// Author:      Nathan Yamashiro
+// Date:        09/13/2024 (Replace xx/xx/xxxx with month/day/year)
 // Class:       CS150-xx (Replace xx with your section number)
-// Assignment:  Hello World
+// Assignment:  Base Converter
 // Purpose:     Display the message Hello World to the screen
 // Hours:       0.25
 //******************************************************************************
@@ -11,10 +11,223 @@
 #include <iostream>
 
 using namespace std;
+int hexCharToInt (char hexDigit);
+char getBase (const string& strNumber);
+string getNumber (const string& prompt);
+void printTitle (const string& myTitle);
+string binaryToDecimal (const string& strNumber);
+string decimalToBinary (const string& strNumber);
+string decimalToHex (const string& strNumber);
+string hexToDecimal (const string& strNumber);
+string hexToBinary (const string& strNumber);
+string binaryToHex (const string& strNumber);
 
+const char BINARY = 'b',
+           HEX = 'x',
+           DECIMAL = 'd';
+
+/**************************************************************************
+ Function: 	 	main
+
+ Description: drive for base converter
+
+ Parameters:	none
+
+ Returned:	 	exit status
+ *************************************************************************/
 int main () {
+  const string QUIT = "q";
+  const string PROMPT = "Enter your string to convert (q to quit): ";
+  string input;
+  char base = 'a';
 
-  cout << "Hello World" << endl;
+  printTitle("*");
+  while (QUIT != input) { 
+    input = getNumber(PROMPT);
+    base = getBase(input);
+    if (base == HEX){
+      // might be able to keep this in the get base function
+      // maybe add a handle hex, bin, int function
+    }
+    else if (base == BINARY){
+
+    }
+    else {
+
+    }
+  }
   
   return EXIT_SUCCESS;
+}
+/**************************************************************************
+ Function: 	 	hexCharToInt
+
+ Description: turns a hex letter value to an integer
+
+ Parameters:	hexDigit - The letter that must be converted
+
+ Returned:	 	number value of the hex letter
+ *************************************************************************/
+int hexCharToInt (char hexDigit) {
+  const int NUM_SHIFT = 11;
+  int num = 0;
+
+  if (hexDigit >= 'A' && hexDigit <= 'F'){ // if digit is a letter
+    num = (static_cast <int> (hexDigit - 'A')) + NUM_SHIFT;
+  }
+  else { // if digit is a number
+    num = static_cast <int> (hexDigit);
+  }
+
+  return num;
+}
+/**************************************************************************
+ Function: 	 	getBase
+
+ Description: finds the base of the string
+
+ Parameters:	strNumber - the string that is checked
+ 
+ Returned:	 	the base of the number
+ *************************************************************************/
+char getBase (const string& strNumber) {
+  
+  char base;
+  if (strNumber[1] == BINARY){
+    base = BINARY;
+  }
+  else if (strNumber[1] == HEX){
+    base = HEX;
+  }
+  else {
+    base = DECIMAL;
+  }
+  return base;
+}
+/**************************************************************************
+ Function: 	 	getNumber
+
+ Description: gets the users input
+
+ Parameters:	prompt - the message printed to the user
+ 
+ Returned:	 	number from user input
+ *************************************************************************/
+string getNumber (const string& prompt) {
+  string num;
+    cout << prompt;
+    cin >> num;
+  return num;
+}
+/**************************************************************************
+ Function: 	 	printTitle
+
+ Description: prints the tile to the screen
+
+ Parameters:	none
+ 
+ Returned:	 	none
+ *************************************************************************/
+void printTitle (const string& myTitle) {
+  cout << "**************************************\n";
+  cout << "*****HEX-DECIMAL-BINARY CONVERTER*****\n";
+  cout << "**************************************\n";
+}
+/**************************************************************************
+ Function: 	 	binaryToDecimal
+
+ Description: converts a binary string to decimal
+
+ Parameters:	strNumber - the string that is converted
+ 
+ Returned:	 	the number after conversion
+ *************************************************************************/
+string binaryToDecimal (const string& strNumber) {
+  string dec;
+  
+  for (int i = 0; i < strNumber.length(); i++) {
+
+  }
+  return dec;
+}
+/**************************************************************************
+ Function: 	 	decimalToBinary
+
+ Description: converts a binary string to decimal
+
+ Parameters:	strNumber - the string that is converted
+ 
+ Returned:	 	the number after conversion
+ *************************************************************************/
+string decimalToBinary (const string& strNumber) {
+  string bin;
+
+  for (int i = 0; i < strNumber.length(); i++) {
+
+  }
+  return bin;
+}
+/**************************************************************************
+ Function: 	 	decimalToHex
+
+ Description: converts a binary string to decimal
+
+ Parameters:	strNumber - the string that is converted
+ 
+ Returned:	 	the number after conversion
+ *************************************************************************/
+string decimalToHex (const string& strNumber) {
+  string hex;
+
+  for (int i = 0; i < strNumber.length(); i++) {
+
+  }
+  return hex;
+}
+/**************************************************************************
+ Function: 	 	hexToDecimal
+
+ Description: converts a binary string to decimal
+
+ Parameters:	strNumber - the string that is converted
+ 
+ Returned:	 	the number after conversion
+ *************************************************************************/
+string hexToDecimal (const string& strNumber) {
+  string dec;
+
+  for (int i = 0; i < strNumber.length(); i++) {
+
+  }
+  return dec;
+}
+/**************************************************************************
+ Function: 	 	hexToBinary
+
+ Description: converts a binary string to decimal
+
+ Parameters:	strNumber - the string that is converted
+ 
+ Returned:	 	the number after conversion
+ *************************************************************************/
+string hexToBinary (const string& strNumber) {
+  string bin;
+  bin = hexToDecimal(strNumber);
+  bin = decimalToBinary (bin);
+  return bin;
+}
+/**************************************************************************
+ Function: 	 	binaryToHex
+
+ Description: converts a binary string to decimal
+
+ Parameters:	strNumber - the string that is converted
+ 
+ Returned:	 	the number after conversion
+ *************************************************************************/
+string binaryToHex (const string& strNumber) {
+  string hex;
+  hex = binaryToDecimal (strNumber);
+  hex = decimalToHex (hex);
+  return hex;
 }
